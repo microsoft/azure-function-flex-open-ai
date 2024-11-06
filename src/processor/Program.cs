2754fcc224b933d577490d0738b0d4de4e02915d
Damien Aicheh <damienaicheh@microsoft.com>
2024-11-06 17:54:15 +0000

    feat: Add metrics and logs.

--- src/processor/Program.cs
+++ src/processor/Program.cs
@@ -1,12 +1,36 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (toRemove is not null)
+            {
+                options.Rules.Remove(toRemove);
+            }
+        });
+    })
+    .ConfigureAppConfiguration((hostContext, config) =>
+    {
+        config.AddJsonFile("host.json", optional: true);
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        logging.AddApplicationInsights(console =>
+        {
+            console.IncludeScopes = true;
+        });
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
     })
     .Build();
 
